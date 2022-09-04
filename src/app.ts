c1c1ee48ba5c7eb50a9318d6fdcb6841ae1560a0
Ivan MSI <arigatory@gmail.com>
2022-09-05 01:10:36 +0300

    fix after eslint

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist')); 
+app.use(express.static('./dist'));
 app.set('view engine', 'pug');
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Chat app listening on port ${PORT}!`);
 });
