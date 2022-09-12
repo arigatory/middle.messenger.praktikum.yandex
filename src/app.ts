79597d55ec7c05247e2f259d9f5bc4f5d4a905e0
Ivan MSI <arigatory@gmail.com>
2022-09-12 08:11:35 +0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./dist'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
