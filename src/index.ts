7f2e0ec674c7f2695282d21bb68b7dce36a78083
Ivan MSI <arigatory@gmail.com>
2022-10-11 21:42:44 +0300

    add router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { LoginPage } from './pages/Login';
 import { RegistrationPage } from './pages/Registration';
+import Router from './utils/Router';
 window.addEventListener('DOMContentLoaded', () => {
-  const root = document.querySelector('#app')!;
-
-  const loginPage = new LoginPage({});
-  const registrationPage = new RegistrationPage({});
-
-  root.append(registrationPage.getContent()!);
-
-  loginPage.dispatchComponentDidMount();
+  
+  Router
+    .use('/', LoginPage)
+    .use('/login', LoginPage)
+    .use('/registration', RegistrationPage)
+    .start();
 });
